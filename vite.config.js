4dcf23485bb6c2ec74f8e9eb7eac7462dcae3b41
Prince Thakur <prince19112003@outlook.com>
2026-02-15 16:43:07 +0530

    'update'

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/My-Portfolio/',
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(),],
   assetsInclude: ['**/*.riv'],
 });
